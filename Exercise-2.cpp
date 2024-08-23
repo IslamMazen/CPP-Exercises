759765f6fdc6e8e0ef53305a6fa15016219ac7e3
IslamMazen <islammazen10@gmail.com>
2024-08-23 22:14:46 +0300

    Create Exercise-2.cpp

---
+++ Exercise-2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Please Enter 3 numbers : \n";
+	int num1,num2,num3,sum;
+	cin >> num1>>num2>>num3;
+	sum = num1 + num2 + num3;
+	cout << "Sum =  " << sum << endl;
+}
