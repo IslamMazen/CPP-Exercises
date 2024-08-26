45a9e6497fbae1abe07770d677465ef24d96e614
IslamMazen <islammazen10@gmail.com>
2024-08-26 21:19:29 +0300

    Create Exercise-4.cpp

---
+++ Exercise-4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int width;
+int height;
+
+cin >> width;
+cin >> height;
+
+int area;
+
+area = width * height;
+
+int premiter;
+
+premiter = width * height *2;
+cout << "Perimeter is " << premiter;
+cout << "\n";
+cout << "Area is:" << area;
+}
