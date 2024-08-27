c63c8a507e26a5761f16545396b8518cf4df217e
IslamMazen <islammazen10@gmail.com>
2024-08-27 21:13:29 +0300

    Create Exercise-8.cpp

---
+++ Exercise-8.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int number;
+	cout << "Enter Number : \n";
+	cin >> number;
+
+	if (number == 1)
+		cout << "one\n";
+	else if (number == 2)
+		cout << "Two\n";
+	else if (number == 3)
+		cout << "Three\n";
+	else if (number == 4)
+		cout << "Four\n";
+	else if (number == 5)
+		cout << "Five\n";
+
+}
