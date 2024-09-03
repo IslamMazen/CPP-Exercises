e8824c2ae685b733a1bb20e37e421e672a0172e0
IslamMazen <islammazen10@gmail.com>
2024-09-03 16:28:40 +0300

    Create Exercise-18.cpp

---
+++ Exercise-18.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   int x;
+   cout << sizeof(int);
+   cout << sizeof(char);
+   cout << sizeof(short);
+   cout << sizeof(long);
+   cout << sizeof(long long);
+   cout << sizeof(float);
+    cout << sizeof(double);
+   cout << sizeof(double long); 
+  cout << sizeof (bool);
+}
