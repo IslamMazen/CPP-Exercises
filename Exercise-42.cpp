0537b64bfbf627fb0fa6a1d1a9a8da2cecc7fa97
IslamMazen <islammazen10@gmail.com>
2024-11-03 20:24:03 +0200

    Create Exercise-42.cpp

---
+++ Exercise-42.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int size, element;
+	cout << "Enter the size of the array: ";
+	cin >> size >> element;
+
+	int* arr = new int[size];
+
+	for (int i = 0; i < size; i++) {
+		cin >> arr[i];
+		if (arr[i] == element) {
+			arr[i] = 0;
+		}
+	}
+		
+	for (int i = 0; i < size;i++) {
+		cout << arr[i];
+	}
+}
